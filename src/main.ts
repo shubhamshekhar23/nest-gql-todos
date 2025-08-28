d3607e0bcf2b367e171c2ecd9343b1d36d87186b
shubhamshekhar23 <shubham.ae.iitkgp@gmail.com>
2025-08-29 01:39:02 +0530

    add: updating console and db

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`===========================`);
+  console.log(`🚀 Server is running on http://localhost:${port}/graphql`);
 }
 bootstrap();
